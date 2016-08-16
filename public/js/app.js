42e88d00596988b467868d40dfac4dc55c63a952
ranvir <ranvirsgorai@gmail.com>
2016-08-16 16:42:51 +0530

    Angular Implimented

--- public/js/app.js
+++ public/js/app.js
@@ -13,12 +13,37 @@ var app=angular.module("ngAngExp",["ui.router"]);
 app.config(function($stateProvider,$urlRouterProvider){
 	$stateProvider
 		.state("app",{
-			url:"",
+			url:"/",
 			templateUrl :"template/main.html",
 			controller: function($scope){
 				$scope.name="Angular Test App";
-			}
+					}
+				})
+		.state("about",{
+			url:"/about",
+			templateUrl :"template/about.html",
+			controller: function($scope){
+				$scope.name="About Page";
+					}
+		})
 
+		.state("about.me",{
+			url:"/me",
+			templateUrl :"template/about-me.html",
+			controller: function($scope){
+				$scope.name="My Page";
+					}
+		})
+
+		.state("about.me.list",{
+			url:"/list",
+			template:"<br>{{name}}</br>",
+			controller: function($scope){
+				$scope.name="My List";
+					}
 		});
-	$urlRouterProvider.otherwise("/")
+
+
+
+ 	$urlRouterProvider.otherwise("/")
 });
